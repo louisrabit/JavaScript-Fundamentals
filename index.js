935e652e0f113643fb66fd9d1390a98398999cf3
luis coelho <luiscoelho18994@gmail.com>
2023-02-20 22:31:26 +0000

    challange 3 , with logical operators and if/else

--- index.js
+++ index.js
@@ -55,3 +55,67 @@ if (markHigherBMI === true){
 console.log(BMIsJohn);
 console.log(BMIsMark);
 console.log(markHigherBMI);
+
+
+
+/* Coding Challenge #3
+There are two gymnastics teams, Dolphins and Koalas. They compete against each 
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, 
+and print it to the console. Don't forget that there can be a draw, so test for that 
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a 
+team only wins if it has a higher score than the other team, and the same time a 
+score of at least 100 points. Hint: Use a logical operator to test for minimum 
+score, as well as multiple else-if blocks �
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when 
+both teams have the same score and both have a score greater or equal 100 
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+GOOD LUCK  */
+
+
+ 
+/* ---------ANSWER------ */
+
+
+/* 1. Calculate the average score for each team, using the test data below
+const scoreDolphins = (96, 108, 89) / 3;
+const scoreKoalas = (88, 91 , 110) / 3;
+
+if (`${scoreDolphins} > ${scoreKoalas}`){
+    console.log(`Dolphis finish with ${scoreDolphins} and Koalas with score of ${scoreKoalas} , the Winner is Dolphins`)
+}else if ( `${scoreDolphins} < ${scoreKoalas}` ){
+    console.log(`Dolphis finish with ${scoreDolphins} and Koalas with score of ${scoreKoalas} , the Winner is Koalas`)
+}else if ( `${scoreDolphins} === ${scoreKoalas}`){
+console.log(`Is a Draw`)
+} */
+
+
+
+//3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a 
+/* team only wins if it has a higher score than the other team, and the same time a 
+score of at least 100 points. Hint: Use a logical operator to test for minimum 
+score, as well as multiple else-if blocks � */
+/* § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123 */
+const scoreDolphins = (97, 112, 101) / 3;
+const scoreKoalas = (100, 95 , 123) / 3;
+
+
+
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100){
+    console.log("Dohphins Win")
+}else if ( scoreDolphins < scoreKoalas ){
+    console.log("Koalas win")
+}else if ( scoreDolphins === scoreKoala  ){
+console.log(`Is a Draw`)
+}
+
+
+
